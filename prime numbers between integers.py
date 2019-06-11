ae3a4df7b836899814b77bf9b051c770ac8e9c38
luckysona <50876072+luckysona@users.noreply.github.com>
2019-06-11 05:36:15 -0700

    Update prime numbers between integers.py

--- prime numbers between integers.py
+++ prime numbers between integers.py
@@ -1,6 +1,13 @@
 s1,s2=map(int,input().split())
-count=0
+s=0
 for i in range(s1,s2+1):
-  if(i%2!=0):
-    count=count+1
-print(count+1)
+    x=bin(i)
+    x=x[2:len(x)]
+    x=x.count("1")
+    s2=0
+    for i in range(1,x):
+        if x%i==0:
+            s2=s2+1
+    if s2==1:
+        s=s+1
+print(s)
