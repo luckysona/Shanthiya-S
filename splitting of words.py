0f225dc45b8f9f2ee12953c9f8dac8683347ae32
luckysona <50876072+luckysona@users.noreply.github.com>
2019-06-15 05:35:15 -0700

    Update splitting of words.py

--- splitting of words.py
+++ splitting of words.py
@@ -1,7 +1,7 @@
 s1,s2=input().split()
 s1=str(s1)  
 s2=int(s2)
-L=[]   
-for i in range(0,len(s1),s2):
-  L.append(s1[i:i+s2])
+L=[] 
+for i in range(len(s1)-s2+1):
+	L.append(s1[i:i+s2])  
 print(*L)
