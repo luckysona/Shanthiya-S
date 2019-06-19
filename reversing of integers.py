7d5c3651955ad4623b9e7545e41932cf7ca346f3
luckysona <50876072+luckysona@users.noreply.github.com>
2019-06-18 23:41:42 -0700

    Update reversing of integers.py

--- reversing of integers.py
+++ reversing of integers.py
@@ -1,4 +1,10 @@
-x=input()  
-y=list(input().split()) 
-y.sort(reverse=True)  
-print("->".join(y))
+n=int(input())
+l1=[]
+l=list(map(int,input().split()))
+for i in range(n-1,-1,-1):
+    l1.append(l[i])
+for i in range(n):
+    if(i!=len(l1)-1):
+        print(l1[i],end='->')
+    else:
+        print(l1[i])
