dd80cda783f82e503fdef8e1c3f53a89ff69f7df
luckysona <50876072+luckysona@users.noreply.github.com>
2019-06-20 22:53:02 -0700

    Update longest common subarray.py

--- longest common subarray.py
+++ longest common subarray.py
@@ -1,20 +1,13 @@
 from difflib import SequenceMatcher 
   
-def longestSubstring(str1,str2): 
- 
-     seqMatch = SequenceMatcher(None,str1,str2) 
-  
-     
-     match = seqMatch.find_longest_match(0, len(str1), 0, len(str2)) 
-  
-    
-     if (match.size!=0): 
-          print (str1[match.a: match.a + match.size])  
-     else: 
+def longestSubstring(s1,s2): 
+      seqMatch = SequenceMatcher(None,s1,s2) 
+      match = seqMatch.find_longest_match(0, len(s1), 0, len(s2)) 
+      if (match.size!=0): 
+          print (s1[match.a: match.a + match.size])  
+      else: 
           print ('No longest common sub-string found') 
-  
-
 if __name__ == "__main__": 
-    str1 = str(input())
-    str2 = str(input())
-    longestSubstring(str1,str2) 
+    s1 = str(input())
+    s2 = str(input())
+    longestSubstring(s1,s2) 
