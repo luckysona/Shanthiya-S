73747bb8b053f736ee6abafc1e7d6df3e9161cb4
luckysona <50876072+luckysona@users.noreply.github.com>
2019-06-26 08:49:46 -0700

    Update eliminating repeating consecutives and reverse.py

--- eliminating repeating consecutives and reverse.py
+++ eliminating repeating consecutives and reverse.py
@@ -1,10 +1,6 @@
-list=list(map(str,input()))  
-i=0
-while(i < len(list)-1):
-    if list[i] == list[i+1]:
-        del list[i]
-        
-    else:
-        i = i+1
-list=list[::-1]
-print("".join(list))
+s1=input()
+s2=''
+for i in s1:
+    if i not in s2:
+        s2+=i 
+print(s2[::-1])
